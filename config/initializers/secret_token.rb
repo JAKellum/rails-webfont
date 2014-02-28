5f9bc4f0912cd34afa025faf92782b210e3627d8
JAKellum <jason.a.kellum@gmail.com>
2014-02-28 11:22:07 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsWebfont::Application.config.secret_key_base = 'ffec221434812245e0d151adf88ce89b55a01f043e4d10134ace6eee97e27fa19a3598cdb10426e3405f585d77f3c18e5dd6ef55b1a764d7f4235fd255c1b8a9'
