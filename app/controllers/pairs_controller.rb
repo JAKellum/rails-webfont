04ed3eac107e7dbcf608b84670847719fb106974
Jason Cummings <jason@kyn.is>
2014-03-10 14:50:09 -0400

    refactored

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,10 +1,7 @@
 class PairsController < ApplicationController
 
   def export
-    # if url is casual
-      @linkpair = Pair.get_link_pair(params[:slider1], params[:slider2], params[:slider3], params[:category_id])
-      # binding.pry
-    # else 
+    @linkpair = Pair.get_link_pair(params[:slider1], params[:slider2], params[:slider3], params[:category_id])
 
 =begin
     name = params[:s1] + ".pdf"
@@ -16,26 +13,21 @@ class PairsController < ApplicationController
 
   def search
     if request.headers.env["HTTP_REFERER"].match('formal')
-      params[:category_id] = Category.where(name: 'formal').first.id.to_i
+      params[:category_id] = Category.formal
     else
-      params[:category_id] = Category.where(name: 'casual').first.id.to_i
+      params[:category_id] = Category.casual
     end
     redirect_to action: 'export', slider1: params[:slider1], slider2: params[:slider2], slider3: params[:slider3], category_id: params[:category_id]
   end
 
-  def category
-    params[:category]
-  end
-
   def casual_pairs
-    category = Category.casual
     render 'mood'
   end
 
-  def formal_pairs
-    category = Category.formal
-    render 'mood'
-  end
+  alias_method :formal_pairs, :casual_pairs
+  # def formal_pairs
+  #   render 'mood'
+  # end
 
   # def selected_pair(selected_category)
   #   @pairs = selected_category.first.pairs
