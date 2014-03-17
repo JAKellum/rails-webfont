1fcd611c4d0d04fb6b2d2cd17b52237ce4b76b7d
Jason Cummings <jason@kyn.is>
2014-03-17 13:09:01 -0400

    Pass pair in session variable. Show selected pair in export view.

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,14 +1,8 @@
 class PairsController < ApplicationController
 
   def export
-    @linkpair = linkpair
+    @linkpair = Pair.find(session[:pair_id])
   end
-=begin
-    name = params[:s1] + ".pdf"
-    Prawn::Document.generate "#{name}" do |pdf|
-      pdf.text @link
-    end
-=end
 
   def results
     @linkpair = Pair.find_by(:slider1 => params[:slider1],
@@ -17,22 +11,8 @@ class PairsController < ApplicationController
                              :category => Category.find_by_name(params[:category]))
 
     linkpair=(@linkpair)
+    session[:pair_id] = @linkpair.id
   end
-
-  def linkpair=(pair)
-    @linkpair = pair
-  end
-
-  def linkpair
-    @linkpair
-  end
-
-=begin
-    name = params[:s1] + ".pdf"
-    Prawn::Document.generate "#{name}" do |pdf|
-      pdf.text @link
-    end
-=end
 
   def search
     redirect_to action: 'results', slider1: params[:slider1], slider2: params[:slider2], slider3: params[:slider3],
--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,18 +1,11 @@
 class Pair < ActiveRecord::Base
   belongs_to :category
 
-
-  def both_fonts
-    [self.headline_font, self.body_font]
-  end
-
   def self.get_link_pair(slider1, slider2, slider3, category)
     Pair.find_by(slider1: slider1, slider2: slider2, slider3: slider3, category_id: category)
   end
 
   def link
-    fonts = self.headline_font + "|" + self.body_font
-    fonts.gsub! ' ', '%20'
-    link = "http://fonts.googleapis.com/css?family=" + fonts
+    "http://fonts.googleapis.com/css?family=" + self.headline_font + "|" + self.body_font
   end
 end
