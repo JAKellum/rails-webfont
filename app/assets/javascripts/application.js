fb846c3e6939a8946e0ebb3e8091ab3891d57078
Paul Matthews <matthewspaulc@gmail.com>
2014-03-19 21:59:42 -0400

    Clean up markup in results page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,48 +16,20 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
-/*function toggleCurrent(button) {
-  $(button).toggleClass('current');
-}*/
+
 $(function() {
   $('li.button.radius').click(function(){
-    $('.current').removeClass('current');
-    $(this).toggleClass('current');
+    var newCurrent = $(this),
+        headlines = $('.h-result'),
+        bodies = $('.b-result');
+
+    $('li.current').removeClass('current');
+    newCurrent.toggleClass('current');
+
+    headlines.css('font-family', newCurrent.attr('data-headline_font'));
+    bodies.css('font-family', newCurrent.attr('data-body_font'));
+    headlines.last().html(newCurrent.attr('data-headline_font'));
+    bodies.last().html(newCurrent.attr('data-body_font'));
   })
 });
-
-$(function() {
-  $('li.button.radius').click(function() {
-    var current = $('.current');
-    $("[id*='alphabet']").hide();
-    current.removeClass('current');
-    $(this).toggleClass('current');
-    $('#alphabet' + current[0].innerHTML).show();
-  });
-});
-
-
-/*
-  $(function() {
-  var urlParams = $(location).attr('href').match(/(casual|formal)|\W\d(\W|$)/g);
-  var replaceContent = function() {
-    $.ajax({
-      type: 'GET',
-      url: 'alphabet' + "?category=" + urlParams[0] + "&slider1=" + urlParams[1].match(/\d/) + "&slider2=" + urlParams[2].match(/\d/) + "&slider3=" + urlParams[3].match(/\d/),
-      //url: 'alphabet' + "?category=" + urlParams[0] + "&slider1=" + (parseInt(urlParams[1].match(/\d/)) + 1) + "&slider2=" + (parseInt(urlParams[2].match(/\d/)) + 2) + "&slider3=" + (parseInt(urlParams[3].match(/\d/)) + 2),
-      dataType: 'html',
-      success : function(data) {
-        var alpha = $(data).find('#alphabet').children();
-        $('#alphabet').html(alpha);
-      }
-    });
-  };
-
-  $('li.button.radius').click(function(){
-    $('.current').removeClass('current');
-    $(this).toggleClass('current');
-    $('#alphabet').html(replaceContent);
-  });
-});
-*/
 
